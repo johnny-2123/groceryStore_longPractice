db61fc93f513210e1980137fb545f5651cd0c471
edgar <johnnyavila2021@u.northwestern.edu>
2023-02-15 21:43:18 -0500

    setting up Redux store index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+
 function Root() {
   return (
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
 }
 
